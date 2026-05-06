cfd1ab039774518ff2675cf59892fcdda2c5dde1
pistabarfy <sahilshaikhfedev@gmail.com>
2026-05-06 23:12:53 +0530

    removed nest.py, was unnecessary

--- nest.py
+++
@@ -1,8 +0,0 @@
-def flatten(*array):
-    result = []
-    for element in array:
-        if isinstance(element, list):
-            # one line here
-        else:
-            # one line here
-    return result
