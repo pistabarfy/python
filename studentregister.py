197a2339e7dc591aa2d5bcac9313a1f3f880dbfc
pistabarfy <sahilshaikhfedev@gmail.com>
2026-05-06 23:06:05 +0530

    day 5 - classes, methods, student register

---
+++ studentregister.py
@@ -0,0 +1,21 @@
+gg = []
+roll = []
+
+while (student := input("Enter the name of the student: ")) != "q" and student != "quit":
+    roll_rank = int(input("Enter the roll number:"))
+    duplicate_status = False
+    for r in roll:
+        if roll_rank== r:
+            print("Dont enter duplicate numbers")
+            duplicate_status = True
+    
+    if duplicate_status == False:
+        gg.append(student)
+        roll.append(roll_rank)
+        for position,value in enumerate(gg):
+            print(f"position is {position} and the name is {value}")
+
+        for name,call in zip(gg,roll):
+            print(f"the name is {name} and its roll number is {call}")
+
+print("Bye!")
