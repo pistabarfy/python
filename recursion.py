ccc320b6cec3c03926a8aa4184ea7ba10755c042
pistabarfy <sahilshaikhfedev@gmail.com>
2026-05-06 23:06:05 +0530

    day 5 - recursion, flattened nested lists manually

---
+++ recursion.py
@@ -0,0 +1,25 @@
+#recursion.py.
+
+def factorial(n):
+    x = "local"
+    print(x)
+    if n == 1:
+        return 1
+    else:
+        factoriyal = n*factorial(n-1)
+        return factoriyal
+print(factorial(5))
+
+x = "global"
+print(x)
+
+def func1():
+    x = "local"
+    print(x)
+
+    def func2():
+        return x
+    
+    func2()
+func1()
+
