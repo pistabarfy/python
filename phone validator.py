9519be8bc3222a752061e07dcba984be3a94cde6
pistabarfy <sahilshaikhfedev@gmail.com>
2026-05-07 18:25:48 +0530

    day 7 - came across the regular expression module, re module. can be very useful for workign with strings and like making very specific conditions to tokenise

---
+++ phone validator.py
@@ -0,0 +1,28 @@
+import re
+phon = input("Enter the ur phone number: ")
+def validator(string):
+
+    if check := re.match(r"^\d{10}$",string) != None:
+        print("The phone number is working")
+    else:
+        print("The phone number isnt working")
+
+    return check
+
+print(validator(phon))
+    
+
+
+
+
+
+        
+
+
+    
+
+    
+        
+
+
+
