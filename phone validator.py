b37206a8ad8afad70b3cd296eb8a9d8c033c3b05
pistabarfy <sahilshaikhfedev@gmail.com>
2026-05-07 19:41:27 +0530

    added an email validation with the help of regex

--- phone validator.py
+++ phone validator.py
@@ -1,15 +1,24 @@
 import re
 phon = input("Enter the ur phone number: ")
+email = input("Enter the email: ")
 def validator(string):
 
     if check := re.match(r"^[6789]\d{9}$",string) != None:
-        print("The phone number is working")
+        print(f"The {string} is a working phone number")
     else:
-        print("The phone number isnt working")
+        print(f"The {string} is a working phone number")
 
     return check
 
+def email_validator(string):
+    if check := re.match(r"^\w[A-Za-z09!#$%&'+-/=?^_]+@+\w+\.\w+$",string) != None:
+        print(f"{string} is a valid email")
+    else:
+        print(f"{string} isnt a valid email")
+    return check
+
 print(validator(phon))
+print(email_validator(email))
     
 
 
