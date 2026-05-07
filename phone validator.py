5515e7ea5f32a2e30d3620eeab7a5d56031f024f
pistabarfy <sahilshaikhfedev@gmail.com>
2026-05-07 18:49:11 +0530

    day 7 - added a condition to only numbers start with 6,7,8,9 to be numbers

--- phone validator.py
+++ phone validator.py
@@ -2,7 +2,7 @@ import re
 phon = input("Enter the ur phone number: ")
 def validator(string):
 
-    if check := re.match(r"^\d{10}$",string) != None:
+    if check := re.match(r"^[6789]\d{9}$",string) != None:
         print("The phone number is working")
     else:
         print("The phone number isnt working")
